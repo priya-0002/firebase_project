8fcb332d058c204f149b69ec962de5ce5bbabe59
priya-002 <priya.md0002@gmail.com>
2021-06-16 21:10:30 +0530

    changes

--- src/components/timelist/Timelist.js
+++ src/components/timelist/Timelist.js
@@ -38,15 +38,7 @@ console.log(times)
         <div>
         
          {/* <div>
-          <label> Sort By:</label> {''}
-          <select>
-        <option>Time(fastest first)</option>
-        <option>Time(slowest first)</option>
-        <option disabled>-----</option>
-        <option>Tittle(a-z)</option>
-        <option>Tittle(z-a)</option>
-          </select>
-         </div>*/}
+       
         <ol>
             {times.map((time)=>
         <li key={time.id}>
